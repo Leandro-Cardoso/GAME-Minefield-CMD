b922c3201ce23294f07ef6627e26b536d52674e4
Leandro Cardoso <leandrocardosodev@gmail.com>
2023-12-08 15:31:48 -0300

    Generate bombs

--- game/__main__.py
+++ game/__main__.py
@@ -1,7 +1,15 @@
+from os import system
+
 from field import Field
 
-minefield = Field(10, 5, 15)
-print(minefield.width, 'x', minefield.heigth)
+system('cls')
+
+minefield = Field(5, 10, 15)
+print(minefield.rows, 'x', minefield.cols)
 print(minefield.bombs)
+
+minefield.generate_field()
+minefield.generate_bombs()
+
 for line in minefield.field:
     print(''.join(line))
--- game/field.py
+++ game/field.py
@@ -1,24 +1,30 @@
+from random import randint
+
 class Field():
-    '''Create field'''
-    def __init__(self, width:int = 100, heigth:int = 100, bombs:int = 3000) -> None:
-        self.width = width
-        self.heigth = heigth
-        self.bombs = width * heigth if bombs > width * heigth else bombs
+    '''Create field.'''
+    def __init__(self, rows:int = 100, cols:int = 100, bombs:int = 3000) -> None:
+        self.rows = rows
+        self.cols = cols
+        self.bombs = rows * cols if bombs > rows * cols else bombs
         self.char = '■'
         self.char_b = 'B'
         self.char_0 = ' '
-        self.field = self.generate_field()
+        self.field = []
     
-    def generate_field(self) -> list:
-        '''Generate field'''
-        line = []
-        for i in range(self.width):
-            line.append(self.char)
-        field = []
-        for i in range(self.heigth):
-            field.append(line)
-        return field
+    def generate_field(self):
+        '''Generate field.'''
+        for row in range(self.rows):
+            cols = []
+            for col in range(self.cols):
+                cols.append(self.char)
+            self.field.append(cols)
     
     def generate_bombs(self):
-        '''Place bombs'''
-        pass
+        '''Place bombs.'''
+        for bomb in range(self.bombs):
+            while True:
+                row = randint(0, self.rows - 1)
+                col = randint(0, self.cols - 1)
+                if self.field[row][col] == self.char:
+                    self.field[row][col] = self.char_b
+                    break
